1fac835d2e286b908d62caef09396959cc75ee2e
choosunsick <random@example.com>
2019-02-11 17:49:51 +0900

    이름 찾아서 단일 팀 만들어 주는 함수 외부 사용 테스트

--- R/only_team.R
+++ R/only_team.R
@@ -5,6 +5,10 @@
 #' @param data KBO data with players from different teams
 #' @param teamname The teamname you want to find
 #' @return The batter data only the team you want to find
+#' @examples
+#' library(kbodatamining)
+#' find_only_team(hanhwa_batter_2018,"한화")
+#' @export
 
 # teamname is string type and only korean baseball team name forexample "한화"
 find_only_team <- function(data,teamname){

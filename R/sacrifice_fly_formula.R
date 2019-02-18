a8b3701557d27841ebc065eae6372134260cb3c3
choosunsick <random@example.com>
2019-02-18 20:31:00 +0900

    희생플라이 함수 추가

--- R/sacrifice_fly_formula.R
+++ R/sacrifice_fly_formula.R
@@ -1,15 +1,15 @@
 #' Sacrifice hit calculate function
 #'
-#' count Sacrifice hit of player
+#' count Sacrifice fly of player
 #'
 #' @param data batter inning data
-#' @return the number of Sacrifice hit
+#' @return the number of Sacrifice fly
 #' @importFrom dplyr select
 #' @examples
 #' ## internal function
-#' #sh_formula(hanhwa_batter_2018)
-sh_formula <- function(data){
+#' #sf_formula(hanhwa_batter_2018)
+sf_formula <- function(data){
   inning <- c('one','two','three','four','five','six','seven','eight', 'nine','ten','eleven','twelve')
   data <- select(data,inning)
-  return(factor_count(data,"41")+factor_count(data,"61"))
+  return(factor_count(data,"50"))
 }

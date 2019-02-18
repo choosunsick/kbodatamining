df8fe79cfbc9562757dc53a9e8298dd2c9e34d36
choosunsick <random@example.com>
2019-02-18 21:11:36 +0900

    obp 함수 추가

---
+++ R/on_base_percentage_formula.R
@@ -0,0 +1,22 @@
+#' On-base percentage calculate function
+#'
+#' Calculate OBP of player
+#'
+#' @param data batter inning data
+#' @return player OBP
+#' @importFrom dplyr select
+#' @examples
+#' ## internal function
+#' #obp_formula(hanhwa_batter_2018)
+obp_formula <- function(data){
+  inning <- c('one','two','three','four','five','six','seven','eight', 'nine','ten','eleven','twelve')
+  data <- select(data,inning)
+  numerator <- sum(data$ab,bb_formula(data),hbp_formula(data))
+  if(numerator!=0){
+    obp <- numerator/sum(numerator,sf_formula(data))
+  }
+  else{
+    obp <- 0
+  }
+  return(obp)
+}

cac513aa313126c87834702f77eb4586854349f4
choosunsick <random@example.com>
2019-02-19 16:29:41 +0900

    투구수 있는 한화 투수 샘플 자료로 변경

--- data-raw/hanhwa_pitcher_2018.R
+++ data-raw/hanhwa_pitcher_2018.R
@@ -4,8 +4,8 @@ sample_set <- fromJSON("./json_sample/Hanhwa_normalseason_2018.json")
 
 hanhwa_pitcher_2018 <- do.call(rbind,lapply(names(sample_set),FUN = function(x)json2pitcherdf(sample_set,x)))
 
-hanhwa_pitcher_2018 <- hanhwa_pitcher_2018[,c(1,5:7,2,8,3,4,9,10:15,18:19,17,16,20,21)]
+hanhwa_pitcher_2018 <- hanhwa_pitcher_2018[,c(1,5:7,2,8,3,4,9,10:15,18:19,17,16,20,21,22)]
 colnames(hanhwa_pitcher_2018) <- c('date','away','home','doubleheader','name','team','position','mound','win','lose',
-                                   'draw','hld','sv','inning','restinning','r','er','k','bb','ab','tbf')
+                                   'draw','hld','sv','inning','restinning','r','er','k','bb','pit','ab','tbf')
 write.csv(hanhwa_pitcher_2018,"data-raw/hanhwa_pitcher_2018.csv",row.names = F)
 usethis::use_data(hanhwa_pitcher_2018,overwrite = TRUE)
